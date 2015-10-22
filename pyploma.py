b556e34288731cbc360c5f19d52c202620e6e46e
Jesús Moreno León <jesus.moreno@programamos.es>
2015-10-22 07:30:13 +0200

    Modify pyploma to generate just 1 certificate: name by parameter

--- pyploma.py
+++ pyploma.py
@@ -6,6 +6,7 @@
 
 print "Pyploma: Diploma generator for LaTeX and pdf.\n"
 
+import sys
 # Invoke terminal
 from commands import *
 import commands
@@ -16,29 +17,32 @@ def run_command(cmd):
 lista = open("listadip", "r").readlines()
 
 #counter
-a = 100
+#a = 100
 
-for line in lista: # for each person in the list
+if len(sys.argv) != 2:
+	print "The name of the certificate must be provided"
+	#return
 
-	a += 1 #counter
-	b = str(a) #transform counter into string
-	salida = open("output" + b + ".tex","w") # create a LaTeX file for each person in the list
+name = str(sys.argv[1])
+filename = name.replace(" ", "")
 
-	text = open("certi.tex") # open the LaTeX document
-	text = text.read() # read it
-	text_list = list(text) # transform it into a list
+salida = open(filename + ".tex","w") # create a LaTeX file for each person in the list
 
-	y_name = text.find("%pointname") #search the point for name inclusion
-	z_name = len("%pointname")+2
-	text_list[y_name+z_name:y_name+z_name] = line # insert the name
+text = open("certi.tex") # open the LaTeX document
+text = text.read() # read it
+text_list = list(text) # transform it into a list
 
-	text_final = "".join(text_list) # from list to string
+y_name = text.find("%pointname") #search the point for name inclusion
+z_name = len("%pointname")+2
+text_list[y_name+z_name:y_name+z_name] = name # insert the name
 
-	salida.write(text_final) # save changes in the created file
-	salida.close() # closes the file
+text_final = "".join(text_list) # from list to string
 
-	run_command(str("pdflatex " + "output" + b + ".tex")) # compile LaTeX a pdf (optional)
-	print line #control
+salida.write(text_final) # save changes in the created file
+salida.close() # closes the file
+
+run_command(str("pdflatex -interaction=nonstopmode " + filename + ".tex")) # compile LaTeX a pdf (optional)
+print name #control
 
 run_command(str("pdftk output*.pdf cat output todos_diplomas.pdf")) # create pdf with all the created diplomas (optional)
 
